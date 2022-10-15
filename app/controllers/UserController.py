e34dbcd9db4ab5a66c63935f6273c02832959f5a
JoseBarrenechea <102694691+JoseBarrenechea@users.noreply.github.com>
2022-10-15 12:38:17 -0500

    Update UserController.py

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -7,22 +7,28 @@ import datetime
 
 def register():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        dni = request.form["dni"]
+        name = request.form["name"]
+        last_name = request.form["last_name"]
+        birthday = request.form["birthday"]
         email = request.form["email"]
-        age = request.form["age"]
-        education = request.form["education"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        
 
         if (len(password) < 8):
             return "Contrasena muy corta"
 
-        user1 = User.query.filter(User.username == username).first()
+        user1 = User.query.filter(User.dni == int(dni)).first()
         user2 = User.query.filter(User.email == email).first()
-
+        
+        if (password != confirm_password):
+            return "Las contrasenas no coinciden"
+        
         if user1 or user2:
             return "Usuario o email ya existe"
 
-        user = User(username=username, password=password, email=email, age=age, education=education)
+        user = User(dni=int(dni), password=password, email=email, name=name,last_name=last_name,birthday = datetime.strptime(date_str, '%m-%d-%Y').date())
 
         try:
             db.session.add(user)
