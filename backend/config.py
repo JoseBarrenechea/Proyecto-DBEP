235caf61552b0a71f50ad32770699c2ede2b7d2b
JoseBarrenechea <jose.barrenechea@utec.edu.pe>
2022-11-20 10:39:54 -0500

    <

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #coger nuestras variables de entorno (en .flaskenv) el URL de conexion para la base de datos
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:79543786@localhost:5432/postgres'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
